d859b24dd4c4b408bffb54f21c978f33014381fe
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2022-10-08 13:11:08 +0200

    Mod settings window now uses tabs

--- Source/Mod/Mod.cs
+++ Source/Mod/Mod.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Verse;
 
@@ -9,6 +11,8 @@ namespace TO.Mod
 	/// </summary>
 	public class Mod : Verse.Mod
 	{
+		private TraderKindCategory _selected = TraderKindCategory.None;
+
 		/// <summary>
 		/// Reads and initializes mod settings.
 		/// </summary>
@@ -27,37 +31,28 @@ namespace TO.Mod
 			return "Trading Options";
 		}
 
-		/// <summary>
-		/// Contents of the mod settings window.
-		/// </summary>
-		/// <param name="inRect">Available area for drawing the settings.</param>
-		public override void DoSettingsWindowContents(Rect inRect)
+		private static void DrawSettings(TraderKindCategory category, Rect settingsArea)
 		{
 			var listing = new Listing_Standard();
-			listing.Begin(inRect);
-
-			foreach (var categoryObj in Enum.GetValues(typeof(TraderKindCategory)))
+			listing.Begin(settingsArea);
+			if (category != TraderKindCategory.None)
 			{
-				var category = (TraderKindCategory) categoryObj;
-				if (category == TraderKindCategory.None)
-				{
-					continue;
-				}
-
 				var categoryName = Enum.GetName(typeof(TraderKindCategory), category);
 
-				listing.Label($"TO_SilverStock{categoryName}".Translate((int) Settings.GetSilverScaling(category)), -1,
-					$"TO_SilverStock{categoryName}Tooltip".Translate());
+				var tooltipRect = listing.Label("TO_SilverStock".Translate((int) Settings.GetSilverScaling(category)));
 				var silverScaling = listing.Slider(Settings.GetSilverScaling(category), Settings.MinSilverScaling,
 					Settings.MaxSilverScaling);
 				Settings.SetSilverScaling(category, silverScaling);
+
+				tooltipRect.height += 22.0f; // Height of an slider.
+				TooltipHandler.TipRegion(tooltipRect, $"TO_SilverStock{categoryName}Tooltip".Translate());
 			}
 
-			listing.Gap();
-			var resetButtonRect = listing.GetRect(30f);
-			var resetWidth = resetButtonRect.width;
-			resetButtonRect.width /= 5f;
-			resetButtonRect.x += resetWidth - resetButtonRect.width;
+			var resetButtonWidth = settingsArea.width / 5.0f;
+			const float resetButtonHeight = 30.0f;
+			var resetButtonX = settingsArea.width - resetButtonWidth - 10.0f;
+			var resetButtonY = settingsArea.height - resetButtonHeight - 10.0f;
+			var resetButtonRect = new Rect(resetButtonX, resetButtonY, resetButtonWidth, resetButtonHeight);
 			if (Widgets.ButtonText(resetButtonRect, "TO_ResetSettings".Translate()))
 			{
 				Settings.Reset();
@@ -65,8 +60,37 @@ namespace TO.Mod
 
 			TooltipHandler.TipRegion(resetButtonRect, "TO_ResetSettingsTooltip".Translate());
 
-
 			listing.End();
+		}
+
+		private List<TabRecord> Tabs()
+		{
+			var tabs = (from TraderKindCategory category in Enum.GetValues(typeof(TraderKindCategory))
+				let categoryName = Enum.GetName(typeof(TraderKindCategory), category)
+				select new TabRecord($"TO_Category{categoryName}".Translate(), () =>
+					{
+						_selected = category;
+						WriteSettings();
+					},
+					category == _selected)).ToList();
+
+			return tabs;
+		}
+
+		/// <summary>
+		/// Contents of the mod settings window.
+		/// </summary>
+		/// <param name="inRect">Available area for drawing the settings.</param>
+		public override void DoSettingsWindowContents(Rect inRect)
+		{
+			const float tabHeight = 30.0f;
+			var tabArea = new Rect(inRect.x, inRect.y + tabHeight, inRect.width, tabHeight);
+			var settingsArea = new Rect(inRect.x, inRect.y + tabHeight, inRect.width, inRect.height - tabHeight);
+
+			Widgets.DrawMenuSection(settingsArea);
+			TabDrawer.DrawTabs(tabArea, Tabs());
+			DrawSettings(_selected, settingsArea.ContractedBy(15.0f));
+
 			base.DoSettingsWindowContents(inRect);
 		}
 	}
