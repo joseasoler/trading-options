b8788de7ab5deb517d0fe1bf4cea15240688956b
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2022-10-09 23:22:38 +0200

    Wealth scaling

--- Source/Harmony/StockGeneratorCount.cs
+++ Source/Harmony/StockGeneratorCount.cs
@@ -1,4 +1,5 @@
-﻿using HarmonyLib;
+﻿using System;
+using HarmonyLib;
 using RimWorld;
 using TO.Mod;
 using Verse;
@@ -8,8 +9,6 @@ namespace TO.Harmony
 	[HarmonyPatch]
 	public static class StockGeneratorCount
 	{
-
-
 		[HarmonyPostfix]
 		[HarmonyPatch(typeof(StockGenerator), "RandomCountOf")]
 		private static void ModifyGeneratedAmounts(ref StockGenerator __instance, ref int __result, ThingDef def)
@@ -17,6 +16,14 @@ namespace TO.Harmony
 			var category = Category.Get(__instance.trader);
 			var scaling = def == ThingDefOf.Silver ? Settings.GetSilverScaling(category) : Settings.GetStockScaling(category);
 			__result = (int) (scaling * __result / 100.0f);
+
+			if (!Settings.GetWealthScaling(category)) return;
+			const double logBase = 100.0;
+			var wealthScale = logBase + Find.World.PlayerWealthForStoryteller;
+			var logWealthScale = Math.Log(wealthScale, logBase);
+			// Make wealth scaling close to 1 with the starting wealth of the vanilla crashlanded/tribal scenarios.
+			var adjustedLogWealthScale = 0.5 * logWealthScale;
+			__result = (int) (__result * adjustedLogWealthScale);
 		}
 	}
 }
