80c4d8d5e4f8c167a8b5ce5b69c07818f480fabe
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2022-10-12 21:27:20 +0200

    Cache wealth scaling calculation results

--- Source/StockScaling.cs
+++ Source/StockScaling.cs
@@ -7,6 +7,21 @@ namespace TO
 {
 	public static class StockScaling
 	{
+		/// <summary>
+		/// Last wealth for which scaling was calculated
+		/// </summary>
+		private static float _lastWealth = -1.0f;
+
+		/// <summary>
+		/// Last wealth scaling value used to calculate full scaling.
+		/// </summary>
+		private static double _lastWealthScaling = -1.0f;
+
+		/// <summary>
+		/// Cached adjustment for the last wealth.
+		/// </summary>
+		private static double _adjustedLogWealthScale;
+
 		public static double Calculate(TraderKindCategory category, ThingDef def, float? wealth)
 		{
 			var linearSetting = def == ThingDefOf.Silver
@@ -21,11 +36,19 @@ namespace TO
 			var wealthScaling = Settings.GetWealthScaling(category);
 			if (wealthScaling <= 0.0 || wealth == null) return linearScaling;
 
+			if (Math.Abs(wealth.Value - _lastWealth) < 0.1f && Math.Abs(wealthScaling - _lastWealthScaling) < 0.000001f)
+			{
+				return _adjustedLogWealthScale;
+			}
+
+			_lastWealthScaling = wealthScaling;
 			var wealthScale = wealthScaling + wealth.Value;
 			var logWealthScale = Math.Log(wealthScale, wealthScaling);
 			var wealthScaleAdjustment = Math.Log(wealthScaling + 10000f, wealthScaling);
-			var adjustedLogWealthScale = Math.Max(1.0, 1.0 + logWealthScale - wealthScaleAdjustment);
-			return linearScaling * adjustedLogWealthScale;
+			_lastWealth = wealth.Value;
+			_adjustedLogWealthScale = Math.Max(1.0, 1.0 + logWealthScale - wealthScaleAdjustment);
+
+			return linearScaling * _adjustedLogWealthScale;
 		}
 	}
 }
