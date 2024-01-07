a7b469f684700d0abe2713601ff62b7f82d17cb9
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2024-01-07 14:47:01 +0100

    Remove year from copyright disclaimers.

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Trading Options")]
-[assembly: AssemblyCopyright("Copyright © 2023 Pathfinding Framework contributors")]
+[assembly: AssemblyCopyright("Copyright © Trading Options contributors")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
